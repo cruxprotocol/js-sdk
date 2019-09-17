9712b9c6e699d1330af0fbd551a14ffd0099d8a7
Yadunandan Batchu <nandubatchu@gmail.com>
2019-09-17 15:45:45 +0530

    gulp changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,22 @@ var gulp = require('gulp');
 const rename = require('gulp-rename');
 const { parallel } = require('gulp');
 
-function config_dev (cb) {
-    gulp.src('./src/config.json')
-        .pipe(rename('config.json'))
-        .pipe(gulp.dest('./lib'));
-    cb();
+function config (cb) {
+    if (process.env.NODE_ENV === "production") {
+        gulp.src('./src/config.prod.json')
+            .pipe(rename('config.json'))
+            .pipe(gulp.dest('./lib'));
+        cb();
+    }
+    else {
+        gulp.src('./src/config.json')
+            .pipe(rename('config.json'))
+            .pipe(gulp.dest('./lib'));
+        cb();
+    }
+    
 };
 
-function config_prod (cb) {
-    gulp.src('./src/config.prod.json')
-        .pipe(rename('config.json'))
-        .pipe(gulp.dest('./lib'));
-    cb();
-};
 
 // exports.config_ = parallel(config_dev)
-exports.config_dev = parallel(config_dev)
-exports.config_prod = parallel(config_prod)
+exports.config = parallel(config)
