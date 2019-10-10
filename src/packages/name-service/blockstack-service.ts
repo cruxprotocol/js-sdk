76711d69f317eeaf4765051b35936abb030fff3a
prakhar-cs <prakhar.sharma@coinswitch.co>
2019-10-10 18:20:11 +0530

    domain_in_header

--- src/packages/name-service/blockstack-service.ts
+++ src/packages/name-service/blockstack-service.ts
@@ -200,6 +200,9 @@ export class BlockstackService extends nameService.NameService {
         }
         const options = {
             baseUrl: this._subdomainRegistrar,
+            headers: {
+                "X-WalletId": this._domain,
+            },
             json: true,
             method: "GET",
             url: `/status/${this._identityCouple.bsId.components.subdomain}`,
@@ -213,6 +216,9 @@ export class BlockstackService extends nameService.NameService {
     public getNameAvailability = async (subdomain: string): Promise<boolean> => {
         const options = {
             baseUrl: this._subdomainRegistrar,
+            headers: {
+                "X-WalletId": this._domain,
+            },
             json: true,
             method: "GET",
             url: `/status/${subdomain}`,
@@ -278,6 +284,7 @@ export class BlockstackService extends nameService.NameService {
             },
             headers: {
                 "Content-Type": "application/json",
+                "X-WalletId": this._domain,
             },
             json: true,
             method: "POST",
