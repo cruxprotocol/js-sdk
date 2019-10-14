3f0543216e3aea7f7d528fd8174df3c05d4f8c83
ramendra2211 <ramendra.singh@coinswitch.co>
2019-10-14 11:30:35 +0000

    client onboarding

--- src/samples/wallet_demo.ts
+++ src/samples/wallet_demo.ts
@@ -1,164 +1,17 @@
-import { CruxClient, IAddressMapping } from "../index";
-import {Decoder, object, optional, string} from "@mojotech/json-type-validation";
+import { CruxClient } from "../index";
 import * as blockstack from "blockstack";
-import { decodeToken, TokenSigner } from 'jsontokens'
-import { async, resolve } from "q";
-// TODO: add optional import statement to use the build
-
-let doc = (document as {
-    getElementById: Function,
-    getElementsByClassName: Function
-})
-
-
-
-// Demo wallet artifacts
-
-let walletClientName = "cruxdev"
-let encryptionKey = "fookey"
-const wallet_btc_address = "1HX4KvtPdg9QUYwQE1kNqTAjmNaDG7w82V"
-const wallet_eth_address = "0x0a2311594059b468c9897338b027c8782398b481"
-
-let sampleAddressMap: IAddressMapping = {
-    BTC: {
-        addressHash: wallet_btc_address
-    },
-    ETH: {
-        addressHash: wallet_eth_address
-    }
-};
-
-let url = new URL(window.location.href);
-encryptionKey = url.searchParams.get("overrideEncryptionKey") || encryptionKey;
-walletClientName = url.searchParams.get("walletClientName") || walletClientName;
-
-doc.getElementById('encryptionKey').textContent = `'${encryptionKey}'`;
-[].forEach.call(doc.getElementsByClassName('walletClientName'), (el: HTMLElement) => { el.textContent = walletClientName })
-doc.getElementById('userAddresses').textContent = Object.keys(sampleAddressMap).map((currency) => { console.log(sampleAddressMap); let address = sampleAddressMap[currency].addressHash; return `${currency.toUpperCase()} - ${address}` }).join('\n')
-
-
-
-
-// --- @crux/js-sdk integration --- //
-
-
-// defining cruxClientOptions
-let cruxClientOptions = {
-    getEncryptionKey: () => encryptionKey,
-    walletClientName: walletClientName
-}
-
-// initialising the cruxClient
-let cruxClient = new CruxClient(cruxClientOptions)
-cruxClient.init().then(async () => {
-    await getCruxIDState()
-    doc.getElementById('init').style.display = "none"
-}).catch((error) => {
-    let message = "CruxClient Initialization Error: \n" + error
-    alert(message)
-    doc.getElementById('init').innerHTML = message
-})
-
-
-// SDK functional interface
+import { TokenSigner } from 'jsontokens'
+import 'babel-polyfill';
 
 
 const ramTest = async () => {
-    alert('ok');
-    // publishClientConfig(clientConfig, getSignPayload);
     getIdStatus('ankit9')
 }
 
-const isCruxIDAvailable = async () => {
-    let UIResponse: string = ""
-    doc.getElementById('availability').textContent = "checking availability ..."
-    let cruxID = doc.getElementById('registrationId').value
-    try {
-        let available = await cruxClient.isCruxIDAvailable(cruxID)
-        UIResponse = available ? "available" : "unavailable"
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('availability').textContent = UIResponse
-    }
-}
-const registerCruxID = async () => {
-    let UIResponse: string = ""
-    let cruxID = doc.getElementById('newSubdomain').value
-    let newAddressMap = sampleAddressMap
-    try {
-        await cruxClient.registerCruxID(cruxID, newAddressMap)
-        UIResponse = "cruxID registration initiated!"
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('registrationAcknowledgement').textContent = UIResponse
-    }
-}
-const resolveCurrencyAddressForCruxID = async () => {
-    let UIResponse: string = ""
-    let cruxID = doc.getElementById('receiverVirtualAddress').value
-    let walletCurrencySymbol = doc.getElementById('currency').value
-    doc.getElementById('addresses').textContent = `resolving cruxID (${cruxID}) ${walletCurrencySymbol} address ...`
-    try {
-        let resolvedAddress = await cruxClient.resolveCurrencyAddressForCruxID(cruxID, walletCurrencySymbol)
-        UIResponse = JSON.stringify(resolvedAddress, undefined, 4)
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('addresses').textContent = UIResponse
-    }
-
-}
-const getAddressMap = async () => {
-    let UIResponse: string = ""
-    try {
-        let addressMap = await cruxClient.getAddressMap()
-        UIResponse = JSON.stringify(addressMap, undefined, 4)
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('addressMap').textContent = UIResponse
-    }    
-}
-const putAddressMap = async () => {
-    let UIResponse: string = ""
-    try {
-        let acknowledgement = await cruxClient.putAddressMap(sampleAddressMap)
-        UIResponse = acknowledgement ? "successfully published addresses!" : acknowledgement.toString()
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('putAddressMapAcknowledgement').textContent = UIResponse
-    }    
-}
-const getCruxIDState = async () => {
-    let UIResponse: string = ""
-    try {
-        let cruxIDStatus = await cruxClient.getCruxIDState()
-        UIResponse = JSON.stringify(cruxIDStatus, undefined, 4)
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('cruxIDStatus').textContent = UIResponse
-    }    
-}
-const updatePassword = async () => { 
-    let UIResponse: string = ""
-    let oldEncryptionKey = doc.getElementById('oldEncryptionKey').value
-    let newEncryptionKey = doc.getElementById('newEncryptionKey').value
-    try {
-        await cruxClient.updatePassword(oldEncryptionKey, newEncryptionKey)
-        UIResponse = 'updated password successfully!'
-    } catch (e) {
-        UIResponse = e
-    } finally {
-        doc.getElementById('passwordUpdateAcknowledgement').textContent = UIResponse
-    }
+const ramTest1 = async () => {
+    publishClientConfig(clientConfig, getSignPayload, "zelcore");
 }
 
-
-// Declaring global variables to be accessible for (button clicks or debugging purposes)
 declare global {
     interface Window {
         wallet: CruxClient;
@@ -170,35 +23,19 @@ declare global {
         getCruxIDState: Function;
         updatePassword: Function;
         ramTest: Function;
+        ramTest1: Function
     }
 }
-
-
-// const signatureCallback = async (payload: {}) => {
-//     // please resolve with signed payload
-// }
 
 const clientConfig = {
     "clientMapping": {
-        "BTC": "1234r",
-        "ETH": "qrtvbf",
-        "LTC": "nbgffvc"
+        "BTC": "ggggg",
+        "ETH": "444444444",
+        "LTC": "3333333"
     }
-}
-const clientMapping = {
-    "BTC": "1234r",
-    "ETH": "qrtvbf",
-    "LTC": "nbgffvc"
-}
-
-export interface IAddress {
-    addressHash: string;
-    secIdentifier?: string;
 }
 
 const uploadClientContentToGaiaHub = async (fileName:string, csAddressMap: object, type: string, gaiaHubUrl: string, publicKey :string, signatureCallback: any, address:any) => {
-    // const hubConfigPromise = connectToGaiaHub(gaiaHubUrl, publicKey, signatureCallback);
-    // const tokenFilePromise = generateTokenFileForContent(csAddressMap, publicKey, signatureCallback);
 
     let [hubConfig, tokenFile] = await getAllSignedPayloads(gaiaHubUrl, csAddressMap, publicKey, signatureCallback, address)
     console.info(hubConfig)
@@ -221,12 +58,12 @@ const uploadClientContentToGaiaHub = async (fileName:string, csAddressMap: objec
         return finalURL;
 }
 
-const publishClientConfig = async (clientConfig, signatureCallback: any) => {
+const publishClientConfig = async (clientConfig: any, signatureCallback: any, publicKey:string, address:string, clientName: string) => {
     const gaiaHubUrl = "https://hub.cruxpay.com"
-    const publicKey = "0363409e301867f3756a66ef4bf0ab91eb96f2cad18e2bd8a49d8726c9f7ff6931"
-    const address = "1Ep3hL7FjMfutRnJfvAAZgzDpQTHKxJZVm"
+    // const publicKey = "0363409e301867f3756a66ef4bf0ab91eb96f2cad18e2bd8a49d8726c9f7ff6931"
+    // const address = "1Ep3hL7FjMfutRnJfvAAZgzDpQTHKxJZVm"
         try {
-            await uploadClientContentToGaiaHub("client-config.json", clientConfig, "application/json", gaiaHubUrl, publicKey, signatureCallback, address);
+            await uploadClientContentToGaiaHub(clientName +"_"+ "client-config.json", clientConfig, "application/json", gaiaHubUrl, publicKey, signatureCallback, address);
         } catch (error) {
             throw new Error(`unable to upload content to gaiahub, ${error}`);
         }
@@ -247,47 +84,6 @@ const getSignPayload = async(payloadArray: any) => {
     const signerKeyHex = "dda3c7f72491f8adf592181370dfa15ebb8057e61eed3329b36f53d92ed5a4b6"
     return [new TokenSigner('ES256K', signerKeyHex).sign(payload1), new TokenSigner('ES256K', signerKeyHex).sign(payload2)]
     // https://github.com/blockstack/blockstack.js/blob/e4864ad41f376d963d4917b95996a6032f992789/src/storage/hub.ts#L173
-}
-
-const getSignedPayloadFromUser = async (payload: object) => {
-    return await getSignPayload(payload);
-}
-
-
-async function makeV1GaiaAuthToken(hubInfo: any, hubUrl: string, publicKey:string, signatureCallback:any): Promise<string> {
-    const challengeText = hubInfo.challenge_text
-    const handlesV1Auth = (hubInfo.latest_auth_version
-    && parseInt(hubInfo.latest_auth_version.slice(1), 10) >= 1)
-    const iss = publicKey;
-
-
-    const salt = new Buffer(crypto.getRandomValues(new Uint32Array(16))).toString('hex')
-    const payload = {
-        gaiaChallenge: challengeText,
-        hubUrl,
-        iss,
-        salt,
-        undefined
-    }
-    const token = await signatureCallback(payload)
-    console.log(token + "===========")
-    return `v1:${token}`
-}
-
-const connectToGaiaHub = async (gaiaHubUrl: string, publicKey: string, signatureCallback: any) => {
-    const response = await fetchPrivate(`${gaiaHubUrl}/hub_info`)
-    const hubInfo = await response.json()
-    console.log(hubInfo);
-    const readURL = hubInfo.read_url_prefix
-    const token = await makeV1GaiaAuthToken(hubInfo, gaiaHubUrl, publicKey, signatureCallback)
-    const address = publicKey
-    console.log(token)
-    return {
-        url_prefix: readURL,
-        address,
-        token,
-        server: gaiaHubUrl
-    }
 }
 
 const getAllSignedPayloads = async (gaiaHubUrl: string, content: object, publicKey: string, signatureCallback: any, address:any) => {
@@ -320,7 +116,7 @@ const getAllSignedPayloads = async (gaiaHubUrl: string, content: object, publicK
 
     const [signedPayload1, signedPayload2] = await signatureCallback([unsignedPayload1, unsignedPayload2])
 
-    console.log(signedPayload1 + "===========")
+    console.log(signedPayload1)
     const token = `v1:${signedPayload1}`
     // Function End
 
@@ -339,24 +135,6 @@ const getAllSignedPayloads = async (gaiaHubUrl: string, content: object, publicK
     const return2 = tokenFile;  
     return [return1, return2]
 }
-
-const generateTokenFileForContent = async (content: object, publicKey: string, signatureCallback: any) => {
-    const payload = {
-        claim: content,
-        issuer: { publicKey },
-        subject: { publicKey },
-    };
-    const token = await signatureCallback(payload)
-    console.log(token + "----------")
-    const tokenFile = [blockstack.wrapProfileToken(token)];
-    return tokenFile;
-}
-
-
-
-
-
-//-------------------------
 
 const parseStatus = (body: any) => {
     let status: any;
@@ -402,17 +180,9 @@ const getIdStatus = async (subdomain: string) => {
     const url = 'https://registrar.coinswitch.co:4000/status/'+subdomain
     const response = await fetchPrivate(`${url}`)
     const status = parseStatus(await response.json())
+    console.log(status)
     return status
 }
 
-
-
-window.wallet = cruxClient;
-window.isCruxIDAvailable = isCruxIDAvailable;
-window.registerCruxID = registerCruxID;
-window.resolveCurrencyAddressForCruxID = resolveCurrencyAddressForCruxID;
-window.getAddressMap = getAddressMap;
-window.putAddressMap = putAddressMap;
-window.getCruxIDState = getCruxIDState;
-window.updatePassword = updatePassword;
 window.ramTest = ramTest;
+window.ramTest1 = ramTest1;
