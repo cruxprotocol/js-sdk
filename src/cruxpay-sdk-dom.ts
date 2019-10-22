26f116f734df06df94f2150ac806e9dd3b3234d0
Sanchay <sanchay.pahalwani@coinswitch.co>
2019-10-22 18:08:00 +0530

    Testing on iOS

--- src/cruxpay-sdk-dom.ts
+++ src/cruxpay-sdk-dom.ts
@@ -1,7 +1,10 @@
-import * as CruxPay from "./index";
-declare global {
-    interface Window {
-        CruxPay: object;
-    }
-}
-window.CruxPay = CruxPay;
+// import * as CruxPay from "./index";
+
+// let CruxPay = CruxPay;
+let footest1 = () => {
+    return "footest1_ABCD";
+};
+global.footest1 = footest1;
+// global.CruxPay = CruxPay;
+
+// node_modules/.bin/browserify src/cruxpay-sdk-dom.ts --standalone CruxFoo > dist/new_bundle.js
