487d5c2c7b310e7fb89f28e4b0b6c40fad1c4d20
ankit <ankitarora121@gmail.com>
2020-01-03 16:40:42 +0530

    add test case stubs for new code

---
+++ src/test/new/application-services.ts
@@ -0,0 +1,12 @@
+describe('Application Services Tests', () => {
+    describe('Testing AssetTranslator', () => {
+
+        it('Translate symbolAddressMap to assetIdAddressMap', ()=>{
+
+        })
+        it('Translate assetIdAddressMap to symbolAddressMap', ()=>{
+
+        })
+
+    })
+});
---
+++ src/test/new/core-entities.ts
@@ -0,0 +1,23 @@
+describe('Core Entities Tests', () => {
+    describe('Testing Entity CruxUser', () => {
+
+        it('CruxUser should not be constructed with invalid address map', ()=>{
+
+        })
+        it('CruxUser getAddress method works as expected', ()=>{
+
+        })
+
+    })
+    describe('Testing Entity CruxDomain', () => {
+
+        it('CruxDomain should not be constructed with invalid config', ()=>{
+
+        })
+        it('CruxDomain should not be constructed with invalid status', ()=>{
+
+        })
+
+    })
+
+});
