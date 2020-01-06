5e40c85fb2b0fcc0f0a97313b10f7f5c1c995b92
ankit <ankitarora121@gmail.com>
2020-01-06 18:39:32 +0530

    Add basic CruxDomain test cases

--- src/test/new/core-entities.ts
+++ src/test/new/core-entities.ts
@@ -1,26 +1,31 @@
+import {CruxDomain, DomainRegistrationStatus} from "../../core/entities/crux-domain";
+import {CruxSpec} from "../../core/entities/crux-spec";
 import {
     CruxUser, IAddress, IAddressMapping,
     ICruxUserRegistrationStatus,
     SubdomainRegistrationStatus,
     SubdomainRegistrationStatusDetail
 } from "../../core/entities/crux-user";
-import {CruxId} from "../../packages/identity-utils";
+import {IClientConfig} from "../../packages/configuration-service";
+import {CruxDomainId, CruxId} from "../../packages/identity-utils";
 import {expect} from 'chai';
 
-const testCruxId = CruxId.fromString('foobar@somewallet.crux');
-const testAddress: IAddress = {
-    'addressHash': 'foobtcaddress'
-};
-const BTC_ASSET_ID: string = 'd78c26f8-7c13-4909-bf62-57d7623f8ee8';
-const testValidAddressMap: IAddressMapping = {[BTC_ASSET_ID]: testAddress};
-const testInvalidAddressMap = {'invalidAssetId': testAddress};
-const newUserRegStatus: ICruxUserRegistrationStatus = {
-    'status': SubdomainRegistrationStatus.NONE,
-    'statusDetail': SubdomainRegistrationStatusDetail.NONE
-};
-
 describe('Core Entities Tests', () => {
+
     describe('Testing Entity CruxUser', () => {
+
+        const testCruxId = CruxId.fromString('foobar@somewallet.crux');
+        const testAddress: IAddress = {
+            'addressHash': 'foobtcaddress'
+        };
+        const BTC_ASSET_ID: string = 'd78c26f8-7c13-4909-bf62-57d7623f8ee8';
+        const testValidAddressMap: IAddressMapping = {[BTC_ASSET_ID]: testAddress};
+        const testInvalidAddressMap = {'invalidAssetId': testAddress};
+        const newUserRegStatus: ICruxUserRegistrationStatus = {
+            'status': SubdomainRegistrationStatus.NONE,
+            'statusDetail': SubdomainRegistrationStatusDetail.NONE
+        };
+
         it('Valid CruxUser can be created', () => {
             const testUser = new CruxUser(testCruxId, testValidAddressMap, newUserRegStatus);
             expect(testUser.cruxID.toString()).to.be.equal(testCruxId.toString());
@@ -37,12 +42,35 @@ describe('Core Entities Tests', () => {
 
     });
     describe('Testing Entity CruxDomain', () => {
-
-        it('CruxDomain should not be constructed with invalid config', () => {
+        const testCruxDomainId = CruxDomainId.fromString('somewallet.crux');
+        const availableDomainStatus: DomainRegistrationStatus = DomainRegistrationStatus.AVAILABLE;
+        const testValidDomainAssetMapping = {
+            'bitcoin': 'd78c26f8-7c13-4909-bf62-57d7623f8ee8'
+        };
+        const testInvalidDomainAssetMapping = {
+            'bitcoin': 'someInvalidAssetId'
+        };
+        const testValidDomainConfig: IClientConfig = {
+            assetMapping: testValidDomainAssetMapping,
+            assetList: CruxSpec.globalAssetList.filter((asset) => Object.values(testValidDomainAssetMapping).includes(asset.assetId)),
+        };
+        const testInvalidDomainConfig: IClientConfig = {
+            assetMapping: testInvalidDomainAssetMapping,
+            assetList: CruxSpec.globalAssetList.filter((asset) => Object.values(testInvalidDomainAssetMapping).includes(asset.assetId)),
+        };
+        it('Valid CruxDomain should be constructed', () => {
+            const testDomain = new CruxDomain(testCruxDomainId, availableDomainStatus, testValidDomainConfig);
+            expect(testDomain);
 
         });
+        it('CruxDomain should not be constructed with invalid asset mapping', () => {
+            const makeInvalidDomain = () => new CruxDomain(testCruxDomainId, availableDomainStatus, testInvalidDomainConfig);
+            expect(makeInvalidDomain).to.throw();
+        });
         it('CruxDomain should not be constructed with invalid status', () => {
-
+            // @ts-ignore
+            const makeInvalidDomain = () => new CruxDomain(testCruxDomainId, 'FOOBAR', testValidDomainConfig);
+            expect(makeInvalidDomain).to.throw();
         });
 
     });
