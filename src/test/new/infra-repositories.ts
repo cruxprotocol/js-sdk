37257b376018a54e6445aa0f516e167ae498aa3a
Yadunandan Batchu <nandubatchu@gmail.com>
2020-01-07 16:56:21 +0530

    mock the class rather than class methods

--- src/test/new/infra-repositories.ts
+++ src/test/new/infra-repositories.ts
@@ -6,6 +6,7 @@ import { CruxSpec } from "../../core/entities/crux-spec";
 import { CruxDomainId } from "../../packages/identity-utils";
 import { DomainRegistrationStatus, CruxDomain } from '../../core/entities/crux-domain';
 import { BasicKeyManager } from '../../infrastructure/implementations/basic-key-manager';
+import * as blkStkService from "../../infrastructure/services/blockstack-service";
 describe('Infrastructure Repositories Test', () => {
     let sandbox: sinon.SinonSandbox;
     before(() => { sandbox = sinon.createSandbox(); })
@@ -30,11 +31,8 @@ describe('Infrastructure Repositories Test', () => {
 
     })
     describe('Testing BlockstackCruxDomainRepository', () => {
+        let mockBlockstackService;
         let blockstackCruxDomainRepository: BlockstackCruxDomainRepository;
-        let getDomainRegistrationStatusStub: sinon.SinonStub;
-        let getClientConfigStub: sinon.SinonStub;
-        let restoreDomainStub: sinon.SinonStub;
-        let putClientConfigStub: sinon.SinonStub;
         // "cruxdev" fixtures
         const cruxdevDomainString = "cruxdev";
         const cruxdevConfigSubdomainPrivateKey = "9d642ba222d8fa887c108472883d702511b9e06d004f456a78d85a740b789dd2";
@@ -50,36 +48,37 @@ describe('Infrastructure Repositories Test', () => {
         // "testcase" fixtures
         const testcaseDomainString = "testcase";
         beforeEach(() => {
+            const getDomainRegistrationStatusStub = sandbox.stub();
+            getDomainRegistrationStatusStub.withArgs(cruxdevDomainString).resolves(DomainRegistrationStatus.REGISTERED);
+            getDomainRegistrationStatusStub.withArgs("testcase").resolves(DomainRegistrationStatus.AVAILABLE);
+
+            const restoreDomainStub = sandbox.stub().resolves(undefined);
+            restoreDomainStub.withArgs(sinon.match(cruxdevConfigKeyManager)).resolves(cruxdevDomainString);
+            
+            mockBlockstackService = {
+                getDomainRegistrationStatus: getDomainRegistrationStatusStub,
+                getClientConfig: sandbox.stub().withArgs(cruxdevDomainString).resolves({assetMapping: cruxdevAssetMapping, assetList: cruxdevAssetList}),
+                restoreDomain: restoreDomainStub,
+            }
+            sandbox.stub(blkStkService, 'BlockstackService').returns(mockBlockstackService);;
             blockstackCruxDomainRepository = new BlockstackCruxDomainRepository({
                 blockstackInfrastructure: CruxSpec.blockstack.infrastructure,
             })
-            // @ts-ignore
-            getDomainRegistrationStatusStub = sandbox.stub(blockstackCruxDomainRepository.blockstackService, 'getDomainRegistrationStatus');
-            // @ts-ignore
-            getClientConfigStub = sandbox.stub(blockstackCruxDomainRepository.blockstackService, 'getClientConfig');
-            // @ts-ignore
-            restoreDomainStub = sandbox.stub(blockstackCruxDomainRepository.blockstackService, 'restoreDomain');
-            // @ts-ignore
-            putClientConfigStub = sandbox.stub(blockstackCruxDomainRepository.blockstackService, 'putClientConfig');
-            getDomainRegistrationStatusStub.withArgs(cruxdevDomainString).resolves(DomainRegistrationStatus.REGISTERED);
-            getDomainRegistrationStatusStub.withArgs("testcase").resolves(DomainRegistrationStatus.AVAILABLE);
-            getClientConfigStub.withArgs(cruxdevDomainString).resolves({assetMapping: cruxdevAssetMapping, assetList: cruxdevAssetList});
-            restoreDomainStub.withArgs(sinon.match(cruxdevConfigKeyManager)).resolves(cruxdevDomainString);
         })
         describe('Finding availability of CruxDomain by DomainId', ()=>{
             it('"cruxdev" should be unavailable', async () => {
                 const domainId = new CruxDomainId(cruxdevDomainString);
                 const availability = await blockstackCruxDomainRepository.find(domainId);
                 expect(availability).to.be.false;
-                expect(getDomainRegistrationStatusStub.calledOnce).to.be.true;
-                expect(getDomainRegistrationStatusStub.calledWithExactly(cruxdevDomainString)).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledOnce).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledWithExactly(cruxdevDomainString)).to.be.true;
             })
             it('"testcase" should be available', async () => {
                 const domainId = new CruxDomainId(testcaseDomainString);
                 const availability = await blockstackCruxDomainRepository.find(domainId);
                 expect(availability).to.be.true;
-                expect(getDomainRegistrationStatusStub.calledOnce).to.be.true;
-                expect(getDomainRegistrationStatusStub.calledWithExactly(testcaseDomainString)).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledOnce).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledWithExactly(testcaseDomainString)).to.be.true;
             })
         })
         describe('Getting the CruxDomain by DomainId', () => {
@@ -89,19 +88,19 @@ describe('Infrastructure Repositories Test', () => {
                 expect(cruxDomain).is.instanceOf(CruxDomain);
                 expect(cruxDomain.status === cruxdevRegistrationStatus).to.be.true;
                 expect(cruxDomain.config).is.eql(cruxdevClientConfig);
-                expect(getDomainRegistrationStatusStub.calledOnce).to.be.true;
-                expect(getDomainRegistrationStatusStub.calledWithExactly(cruxdevDomainString)).to.be.true;
-                expect(getClientConfigStub.calledOnce).to.be.true;
-                expect(getClientConfigStub.calledWithExactly(cruxdevDomainString)).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledOnce).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledWithExactly(cruxdevDomainString)).to.be.true;
+                expect(mockBlockstackService.getClientConfig.calledOnce).to.be.true;
+                expect(mockBlockstackService.getClientConfig.calledWithExactly(cruxdevDomainString)).to.be.true;
 
             })
             it('"testcase" should resolve undefined', async () => {
                 const domainId = new CruxDomainId(testcaseDomainString);
                 const cruxDomain = await blockstackCruxDomainRepository.get(domainId);
                 expect(cruxDomain).is.undefined;
-                expect(getDomainRegistrationStatusStub.calledOnce).to.be.true;
-                expect(getDomainRegistrationStatusStub.calledWithExactly(testcaseDomainString)).to.be.true;
-                expect(getClientConfigStub.notCalled).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledOnce).to.be.true;
+                expect(mockBlockstackService.getDomainRegistrationStatus.calledWithExactly(testcaseDomainString)).to.be.true;
+                expect(mockBlockstackService.getClientConfig.notCalled).to.be.true;
             })
         })
         describe('Getting a CruxDomain with configKeyManager', () => {
@@ -110,18 +109,18 @@ describe('Infrastructure Repositories Test', () => {
                 expect(cruxDomain).is.instanceOf(CruxDomain);
                 expect(cruxDomain.status === cruxdevRegistrationStatus).to.be.true;
                 expect(cruxDomain.config).is.eql(cruxdevClientConfig);
-                expect(restoreDomainStub.calledOnce).to.be.true;
-                expect(restoreDomainStub.calledWith(sinon.match(cruxdevConfigKeyManager))).to.be.true;
-                expect(getClientConfigStub.calledOnce).to.be.true;
-                expect(getClientConfigStub.calledWithExactly(cruxdevDomainString)).to.be.true;
+                expect(mockBlockstackService.restoreDomain.calledOnce).to.be.true;
+                expect(mockBlockstackService.restoreDomain.calledWith(sinon.match(cruxdevConfigKeyManager))).to.be.true;
+                expect(mockBlockstackService.getClientConfig.calledOnce).to.be.true;
+                expect(mockBlockstackService.getClientConfig.calledWithExactly(cruxdevDomainString)).to.be.true;
             })
             it('No domain available on the configKeyManager provided', async () => {
                 const randomKeyManager = new BasicKeyManager("392555374ccf6c13a3f0a794dc94658861fe4a1c568169eb8bdf89c421968023");
                 const cruxDomain = await blockstackCruxDomainRepository.getWithConfigKeyManager(randomKeyManager);
                 expect(cruxDomain).is.undefined;
-                expect(restoreDomainStub.calledOnce).to.be.true;
-                expect(restoreDomainStub.calledWith(sinon.match(randomKeyManager))).to.be.true;
-                expect(getClientConfigStub.notCalled).to.be.true;
+                expect(mockBlockstackService.restoreDomain.calledOnce).to.be.true;
+                expect(mockBlockstackService.restoreDomain.calledWith(sinon.match(randomKeyManager))).to.be.true;
+                expect(mockBlockstackService.getClientConfig.notCalled).to.be.true;
             })
         })
         describe('Saving changes to a CruxDomain', ()=>{
@@ -140,12 +139,12 @@ describe('Infrastructure Repositories Test', () => {
                 const domainId = new CruxDomainId(cruxdevDomainString);
                 const cruxDomain = await blockstackCruxDomainRepository.get(domainId);
                 cruxDomain.config.assetMapping = newAssetMap;
-                putClientConfigStub.withArgs(cruxdevDomainString, cruxDomain.config, cruxdevConfigKeyManager).resolves(cruxDomain);
+                mockBlockstackService.putClientConfig = sandbox.stub().withArgs(cruxdevDomainString, cruxDomain.config, cruxdevConfigKeyManager).resolves(cruxDomain);
                 const updatedCruxDomain = await blockstackCruxDomainRepository.save(cruxDomain, cruxdevConfigKeyManager);
                 expect(updatedCruxDomain).to.be.instanceOf(CruxDomain);
                 expect(updatedCruxDomain.config.assetMapping['btc']).to.be.equal("d78c26f8-7c13-4909-bf62-57d7623f8ee8");
-                expect(putClientConfigStub.calledOnce).to.be.true;
-                expect(putClientConfigStub.calledWith(cruxdevDomainString, sinon.match({assetMapping: newAssetMap, assetList: newAssetList}), sinon.match(cruxdevConfigKeyManager))).to.be.true;
+                expect(mockBlockstackService.putClientConfig.calledOnce).to.be.true;
+                expect(mockBlockstackService.putClientConfig.calledWith(cruxdevDomainString, sinon.match({assetMapping: newAssetMap, assetList: newAssetList}), sinon.match(cruxdevConfigKeyManager))).to.be.true;
             })
         })
 
