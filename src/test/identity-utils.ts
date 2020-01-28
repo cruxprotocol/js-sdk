4603821c701320216121c38f47476b138ed70db8
Sanchay <sanchay.pahalwani@gmail.com>
2020-01-28 17:57:39 +0530

    Added identity-util test cases

--- src/test/identity-utils.ts
+++ src/test/identity-utils.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
-import {BlockstackId, CruxId, IdTranslator, validateSubdomain} from "../packages/identity-utils";
-import { PackageErrorCode } from '../packages/error';
+import {BlockstackId, CruxId, IdTranslator, validateSubdomain, BlockstackDomainId, CruxDomainId} from "../packages/identity-utils";
+import { PackageErrorCode, PackageError } from '../packages/error';
 
 // TODO: strict validation of the cruxID format
 
@@ -14,10 +14,60 @@ describe('ID Translation Tests', () => {
 
     let testBlockstackId3 = 'ankit.something_crux.foo'
 
+    let testBlockstackDomain = 'exodus_crux'
+    let testCruxDomainId = 'exodus.crux'
+
+    let testBlockstackId4 = 'foobar.id'
+    let testInvalidNamespaceId = 'alice.foo'
+
+    it('blockstack id initialization without subdomain', () => {
+        let bsid = BlockstackId.fromString(testBlockstackId4)
+        expect(bsid).to.be.instanceOf(BlockstackId);
+    })
+
+    it('BlockstackId.fromString should fail with BlockstackIdInvalidStructure', () => {
+        let raisedError: Error;
+        try {
+            BlockstackId.fromString("alice")
+        } catch (error) {
+            raisedError = error;
+        }
+        expect(raisedError).to.be.instanceOf(PackageError);
+        expect(raisedError["errorCode"]).to.be.equal(PackageErrorCode.BlockstackIdInvalidStructure);
+    })
 
     it('blockstack id translates to crux id', () => {
         let bsid = BlockstackId.fromString(testBlockstackId1)
         expect(IdTranslator.blockstackToCrux(bsid).toString()).to.equal(testCruxId1)
+    })
+
+    it('blockstack domain id translates to crux domain id', () => {
+        const bsDomainId = new BlockstackDomainId(testBlockstackDomain);
+        const cruxDomainId = IdTranslator.blockstackToCrux(bsDomainId);
+        expect(cruxDomainId).to.be.instanceOf(CruxDomainId);
+        expect(IdTranslator.blockstackToCrux(bsDomainId).toString()).to.equal(testCruxDomainId);
+    })
+
+    it('CruxDomainId.fromString should fail with CruxDomainInvalidStructure', () => {
+        let raisedError: Error;
+        try {
+            CruxDomainId.fromString("alice")
+        } catch (error) {
+            raisedError = error;
+        }
+        expect(raisedError).to.be.instanceOf(PackageError);
+        expect(raisedError["errorCode"]).to.be.equal(PackageErrorCode.CruxDomainInvalidStructure);
+    })
+
+    it('CruxDomainId.fromString should fail with CruxDomainNamespaceValidation', () => {
+        let raisedError: Error;
+        try {
+            CruxDomainId.fromString(testInvalidNamespaceId)
+        } catch (error) {
+            raisedError = error;
+        }
+        expect(raisedError).to.be.instanceOf(PackageError);
+        expect(raisedError["errorCode"]).to.be.equal(PackageErrorCode.CruxDomainNamespaceValidation);
     })
 
     it('crux id translates to blockstack id', () => {
