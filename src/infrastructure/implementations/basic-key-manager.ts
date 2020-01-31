2329ac4adf95a4a46215fe50c97f831ea78a3bfd
Yadunandan Batchu <nandubatchu@gmail.com>
2020-01-31 18:02:27 +0530

    encrypt the privateKey in-memory

--- src/infrastructure/implementations/basic-key-manager.ts
+++ src/infrastructure/implementations/basic-key-manager.ts
@@ -1,22 +1,47 @@
 import { TokenSigner } from "jsontokens";
 import { IKeyManager } from "../../core/interfaces/key-manager";
+import { Encryption } from "../../packages/encryption";
 import { getLogger } from "../../packages/logger";
-import { getKeyPairFromPrivKey } from "../../packages/utils";
+import { getKeyPairFromPrivKey, getRandomHexString } from "../../packages/utils";
 const log = getLogger(__filename);
 export class BasicKeyManager implements IKeyManager {
-    private privateKey: string;
-    private publicKey: string;
-    constructor(privateKey: string) {
-        const keyPair = getKeyPairFromPrivKey(privateKey);
-        this.privateKey = keyPair.privKey;
-        this.publicKey = keyPair.pubKey;
+    private getEncryptionKey?: () => Promise<string>;
+    private ephemeralEncryptionConstant?: string;
+    private encryptedPrivateKey!: string;
+    private publicKey!: string;
+    constructor(privateKey: string, getEncryptionKey?: () => Promise<string>) {
+        this.setEncryptedPrivateKey(privateKey, getEncryptionKey);
         log.debug("BasicKeyManager initialised");
     }
     public signWebToken = async (payload: any): Promise<string> => {
-        const signedMsg = new TokenSigner("ES256K", this.privateKey).sign(payload);
+        const signedMsg = new TokenSigner("ES256K", await this.getDecryptedPrivateKey()).sign(payload);
         return signedMsg;
     }
     public getPubKey = async (): Promise<string> => {
         return this.publicKey;
+    }
+    private setEncryptedPrivateKey = async (privateKey: string, getEncryptionKey?: () => Promise<string>) => {
+        let encryptionConstant: string;
+        if (getEncryptionKey) {
+            encryptionConstant = await getEncryptionKey();
+            this.getEncryptionKey = getEncryptionKey;
+        } else {
+            encryptionConstant = getRandomHexString();
+            this.ephemeralEncryptionConstant = encryptionConstant;
+        }
+        const keyPair = getKeyPairFromPrivKey(privateKey);
+        this.publicKey = keyPair.pubKey;
+        this.encryptedPrivateKey = JSON.stringify(await Encryption.encryptText(keyPair.privKey, encryptionConstant));
+    }
+    private getDecryptedPrivateKey = async (): Promise<string> => {
+        let encryptionConstant: string;
+        if (this.getEncryptionKey) {
+            encryptionConstant = await this.getEncryptionKey();
+        } else {
+            // assume that there is ephemeralEncryptionConstant set as fallback
+            encryptionConstant = this.ephemeralEncryptionConstant!;
+        }
+        const encryptedPrivateKeyObject: { encBuffer: string, iv: string } = JSON.parse(this.encryptedPrivateKey);
+        return Encryption.decryptText(encryptedPrivateKeyObject.encBuffer, encryptedPrivateKeyObject.iv, encryptionConstant);
     }
 }
