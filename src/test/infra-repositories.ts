f49b4d119712a6d25337f4d6f2785fad04b6252f
Sanchay <sanchay.pahalwani@gmail.com>
2020-02-03 18:54:57 +0530

    Fixed test cases

--- src/test/infra-repositories.ts
+++ src/test/infra-repositories.ts
@@ -109,7 +109,7 @@ describe('Infrastructure Repositories Test', () => {
             expect(cruxUserAvailable).is.eql(true);
         })
         it('Getting registered CruxUser by ID', async ()=>{
-            mockBlockstackService.getCruxIdInformation.withArgs(testUserCruxId).resolves({
+            mockBlockstackService.getCruxIdInformation.withArgs(testUserCruxId, false).resolves({
                 ownerAddress: testUserNameDetails.address,
                 registrationStatus: {
                     status: SubdomainRegistrationStatus.DONE,
@@ -141,10 +141,10 @@ describe('Infrastructure Repositories Test', () => {
                 },
                 transactionHash: testUserNameDetails.last_txid,
             })
-            expect(mockBlockstackService.getCruxIdInformation.calledOnceWithExactly(testUserCruxId)).to.be.true;
+            expect(mockBlockstackService.getCruxIdInformation.calledOnceWithExactly(testUserCruxId, false)).to.be.true;
         })
         it('Getting registered CruxUser by ID with tag', async ()=>{
-            mockBlockstackService.getCruxIdInformation.withArgs(testUserCruxId).resolves({
+            mockBlockstackService.getCruxIdInformation.withArgs(testUserCruxId, false).resolves({
                 ownerAddress: testUserNameDetails.address,
                 registrationStatus: {
                     status: SubdomainRegistrationStatus.DONE,
@@ -175,7 +175,7 @@ describe('Infrastructure Repositories Test', () => {
                     "addressHash":"1HX4KvtPdg9QUYwQE1kNqTAjmNaDG7w82V",
                 }
             });
-            expect(mockBlockstackService.getCruxIdInformation.calledOnceWithExactly(testUserCruxId)).to.be.true;
+            expect(mockBlockstackService.getCruxIdInformation.calledOnceWithExactly(testUserCruxId, false)).to.be.true;
         })
         it('Getting unregistered CruxUser by ID', async ()=>{
             mockBlockstackService.getCruxIdInformation.withArgs(unregisteredCruxId).resolves({
