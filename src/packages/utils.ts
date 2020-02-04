86af5236cec8577af24de959835b96f452580b47
Yadunandan Batchu <nandubatchu@gmail.com>
2020-02-04 11:58:52 +0530

    remove redundant util

--- src/packages/utils.ts
+++ src/packages/utils.ts
@@ -139,23 +139,7 @@ const getKeyPairFromPrivKey = (privKey: string): {
     };
 };
 
-const clearObject = (obj: any) => {
-    for (const key in obj) {
-        if (obj.hasOwnProperty(key)) {
-            // @ts-ignore
-            if (typeof obj[key] === "string") {
-                obj[key] = "0".repeat(obj[key].length);
-            } else {
-                obj[key] = "0";
-            }
-            delete obj[key];
-        }
-    }
-    obj = undefined;
-};
-
 export {
-    clearObject,
     httpJSONRequest,
     sanitizePrivKey,
     cachedFunctionCall,
