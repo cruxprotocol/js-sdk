70a68841f7bcffb2d8b4152c588c346471ae0f55
ankit <ankitarora121@gmail.com>
2020-05-07 16:10:07 +0530

    Rearrange for clarity

---
+++ src/core/domain-services/index.ts
@@ -0,0 +1,1 @@
+export * from "./crux-gateway";
--- src/core/interfaces/crux-gateway.ts
+++ src/core/interfaces/crux-gateway.ts
@@ -2,14 +2,19 @@ import {CruxId} from "../../packages";
 import {CruxGateway} from "../entities";
 import {IKeyManager} from "./key-manager";
 
+/**
+ * Used to assert ownership of a CruxID
+ */
 export interface IGatewayIdentityClaim {
     cruxId: CruxId;
     keyManager: IKeyManager;
 }
 
+/**
+ * Asserts rules to a message's content
+ */
 export interface IGatewayProtocolHandler {
     getName(): string;
-
     validateMessage(gatewayMessage: any): boolean;
 }
 
@@ -20,4 +25,20 @@ export interface ICruxGatewayRepository {
 export interface IPubSubProvider {
     subscribe: (topic: string, callback: any) => void;
     publish: (topic: string, data: any) => void;
+}
+
+export interface IGatewayPacket {
+    metadata: IGatewayPacketMetadata;
+    message: any;
+}
+
+export interface IGatewayPacketMetadata {
+    packetCreatedAt: Date;
+    protocol: string;
+    senderCertificate?: IGatewayIdentityCertificate;
+}
+
+export interface IGatewayIdentityCertificate {
+    claim: string;
+    proof: string;
 }
