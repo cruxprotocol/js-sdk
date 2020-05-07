14827c06119fa2a5981c741e714c6aae423b430a
ankit <ankitarora121@gmail.com>
2020-05-07 14:40:51 +0530

    Add basic working test case

--- src/test/crux-gateway-entity.ts
+++ src/test/crux-gateway-entity.ts
@@ -8,18 +8,20 @@ describe('CRUX Gateway Entity Tests', async function() {
         this.timeout(1000)
         this.inmemoryGatewayRepo = new InMemoryCruxGatewayRepository();
 
-        const user1 = getValidCruxUser();
-        const user2 = getValidCruxUser2();
+        this.user1 = getValidCruxUser();
+        this.user2 = getValidCruxUser2();
 
-        this.user1IDClaim = getIdClaimForUser(user1);
-        this.user2IDClaim = getIdClaimForUser(user2)
+        this.user1IDClaim = getIdClaimForUser(this.user1);
+        this.user2IDClaim = getIdClaimForUser(this.user2)
+
     });
     it('New private address addition for self and resolution works properly', async function() {
-        const user1Gateway: CruxGateway = this.inmemoryGatewayRepo.openGateway('BASIC', this.user1IDClaim);
-        const user2Gateway: CruxGateway = this.inmemoryGatewayRepo.openGateway('BASIC', this.user2IDClaim);
-        user2Gateway.listen((md, msg)=>{
+        this.user1Gateway = this.inmemoryGatewayRepo.openGateway('BASIC', this.user1IDClaim);
+        this.user2Gateway = this.inmemoryGatewayRepo.openGateway('BASIC', this.user2IDClaim);
+
+        this.user2Gateway.listen((md: any, msg: any)=>{
             console.log("User 2 Received Message ", md, msg);
         });
-        user1Gateway.sendMessage(this.user2IDClaim.cruxId, "YOLO");
+        this.user1Gateway.sendMessage(this.user2.cruxID, "YOLO");
     });
 });
