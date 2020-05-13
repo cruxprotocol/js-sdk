f7af8a8e8b6c96e72655fdde55f055c960914af9
ankit <ankitarora121@gmail.com>
2020-05-13 20:24:01 +0530

    Remove unused testcase

--- src/test/crux-messenger/test-cruxid-messenger.ts
+++
@@ -1,7 +0,0 @@
-import {InMemoryPubSubClientFactory} from "./inmemory-implementations";
-
-describe('Testing CruxID Messenger', function() {
-    beforeEach(async function() {
-        this.pubsubClientFactory = new InMemoryPubSubClientFactory();
-    });
-})
